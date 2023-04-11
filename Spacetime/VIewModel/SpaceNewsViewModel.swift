c07ade3985cd1984f1607c93c4548833cdd815f7
Scarlett Vu <uscarlettv@gmail.com>
2023-04-11 02:53:10 -0700

    Spacenews app with implemented news API

--- Spacetime/VIewModel/SpaceNewsViewModel.swift
+++ Spacetime/VIewModel/SpaceNewsViewModel.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 class ViewModel:ObservableObject{
-    @Published var news:[SpacetimeNews] = []
+    @Published var news: [SpacetimeNews] = []
     @Published var url = "https://api.spaceflightnewsapi.net/v3/articles"
     func fetchData(){
-        guard let url = URL(string: url)else{return}
-        let sess = URLSession.shared.dataTask(with: url){[weak self] data, _ ,error in
+        guard let url = URL(string: url) else {return}
+        let sess = URLSession.shared.dataTask(with: url) {[weak self] data, _ ,error in
             
             guard let data = data, error == nil else {
                 return
